1a0825ebdfc14ba459684d9a16423c007acc5bc5
stevie_s30 <steve@enterphin.com>
2020-07-22 18:06:11 +0900

    Create, read, update operation on user api

--- intranet-server/app.js
+++ intranet-server/app.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 require('./src/db/mongoose');
+const userRouter = require('./src/routes/userRoutes');
 const app = express();
 const port = 3000 || process.env.port;
 
@@ -8,6 +9,7 @@ const publicDirectory = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectory));
 app.use(express.json());
+app.use(userRouter);
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
--- intranet-server/src/model/user.js
+++ intranet-server/src/model/user.js
@@ -6,25 +6,52 @@ const jwt = require('jsonwebtoken');
 const userSchema = new mongoose.Schema({
     kr_name: {
         type: String,
-        required: true
+        required: true,
     },
     en_name: {
         type: String, 
         required: true
     },
     email: {
-        type: String, 
+        type: String,
+        unique: true, 
         required: true,
+        trim: true,
+        toLowerCase: true,
         validate(value){
-            if(!isEmail(value)){
-                throw new Error;
+            if(!validator.isEmail(value)){
+                throw new Error('Email is not valid');
             }
         }
     },
     password: {
         type: String,
         required: true,
-        trim: true,
+        trim: true
     },
-    
-})
+    department: {
+        type: String,
+        enum: [ '사업부', '연구소' ],
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true,
+        validate(value){
+            if(!validator.isMobilePhone(value)){
+                throw new Error('Phone number is not valid. Mobile phone number is required.');
+            }
+        }
+    },
+    avatar: {
+        type: Buffer
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
